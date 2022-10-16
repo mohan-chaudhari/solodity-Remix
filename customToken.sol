017036f5a8112df39aebb08857ab083bd74bc76b
Remix Workspace <>
2022-10-16 23:11:34 +0530

    erc20 using openzeppelin

---
+++ customToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract CustomToken is ERC20{
+
+    constructor(uint256 initialSupply) ERC20 ("CustomToken", "MOHAN"){
+        _mint(msg.sender, initialSupply);
+    }
+
+    function decimals() public view virtual override returns(uint8){
+        return 6;
+    }
+}
